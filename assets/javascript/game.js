8b95e15680388a2d3a7f2189bf3bd08832b05ba4
Morgan Styers <morganstyers@gmail.com>
2019-08-02 17:23:08 -0400

    enter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,59 @@
 // variables to keep track of points, guessed letters and tries remaining
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft = 9
 var attemptList = [];
-var userGuess=  [];
+
+
+
 //variables indicating array possibilities and restrictions
+
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userInput = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(compGuess);
 
+
+
+
 document.onkeyup = function (event) {
     var userGuess = event.key;
+    if (userInput.indexOf(guessesLeft) > -1) {
 
-    attemptList.push(userGuess);
-
-    var reset = function () {
-        numGuessesLeft = 10;
+   reset = function () {
+        gessesLeft = 10;
         attemptList = [];
     };
-    
 
     if (userGuess === compGuess) {
         alert('Hurrayyyyy');
         wins++;
         document.getElementById("winCount").innerHTML = "Wins: " + wins;
-        reset();
-
+        GuessesLeft=9;
+        attemptList=[];
+        
+    }
+    if (userGuess != compGuess){
+        GuessesLeft--;
+        userInput.push(userGuess);
     }
 
-    else {
-        losses++;
-        numGuessesLeft--;
-    }
-
-    if (numGuessesLeft === 0) {
+    if (guessesLeft === 0) {
         alert('nooooooooooooo');
         losses++;
-        document.getElementById("lossCount").innerHTML = "Losses: " + losses;
-        alert('nooooooooooooo')
-        numGuessesLeft = 9;
-        reset();
+        attemptList=[];
     };
 
 
 
-}
+var html= 
+       "<p> Wins: "+ wins+ "</p>" +
+        "<p>Losses: "+losses+ "</p>" +
+        "<p>Guesses Left: "+ guessesLeft + "</p>"+
+        "<p>Your Guesses So Far: "+ userInput.join(",")+"</p>"
 
-function numGuessesLeft() {
+        document.getElementById("stats").innerHTML = html;
 
-}
-function attemptList() {
-
-    var html = "<p>WinCount: " + Win + "</p>" + "<p>LossCount " + Lose + "<p> guessCount " + numGuessesLeft + "<p>Your Guesses so far: " + attemptList.join(', '); //Reveal guessed letters here
-    document.getElementById("print-content").innerHTML = html;
+};
 }
